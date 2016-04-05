9a75d9a9ef41eff104a5a91306459c1ae8d35819
Daniel Royde <danielroyde@gambitresearch.com>
2016-04-05 15:51:41 +0100

    Reduce amount of state in the stack.

--- src/puzzle3.py
+++ src/puzzle3.py
@@ -21,29 +21,8 @@ class GameStateTuple(object):
 
 
 class GameState(GameStateTuple):
-	MAIN_STATE_NAMES = ('die_north_index', 'die_top_index', 'die_location')
-
 	def __str__(self):
 		return 'Die:%s\nScore:%s\nVisited:\n%s' % (str(self.die_face_values), self.score(), str(self.visited))
-
-	def modify(self, modifier):
-		self.die_north_index.append(modifier.die_north_index)
-		self.die_top_index.append(modifier.die_top_index)
-		self.die_location.append(modifier.die_location)
-
-		row_col, die_face_index = modifier.visited
-		self.visited[row_col] = die_face_index
-		if modifier.die_face_values:
-			face_index, face_value = modifier.die_face_values
-			self.die_face_values[face_index] = face_value
-
-	def unmod(self, modifier):
-		self.die_north_index.pop()
-		self.die_top_index.pop()
-		self.die_location.pop()
-		self.visited.popitem()
-		if modifier.die_face_values:
-			self.die_face_values.popitem()
 
 	def __cmp__(self, other):
 		if other is None:
@@ -56,7 +35,7 @@ class GameState(GameStateTuple):
 				tuple(self.die_top_index) == tuple(other.die_top_index) and
 				self.visited == other.visited and self.die_face_values == other.die_face_values)
 
-	#@functools32.lru_cache(1)
+	@functools32.lru_cache(1)
 	def score(self):
 		product = 1
 		for die_face_index in self.visited.values():
@@ -65,6 +44,7 @@ class GameState(GameStateTuple):
 				space_value = 9
 			product *= space_value
 		return product
+
 
 class GameStateModifier(GameStateTuple):
 	pass
@@ -77,7 +57,7 @@ class GameStatic(object):
 		self.end_location = len(board_values) - 1, len(board_values[0]) - 1
 
 	def next_state_modifiers(self, game_state):
-		row, col = game_state.die_location[-1]
+		row, col = game_state.die_location
 		modifiers = []
 		for move, (row_move, col_move) in MOVES.iteritems():
 			new_row = row + row_move
@@ -89,20 +69,19 @@ class GameStatic(object):
 			board_value = self.board[new_row][new_col]
 			die_top_index, die_north_index = self._die.result_of_move(
 				move,
-				top_index=game_state.die_top_index[-1],
-				north_index=game_state.die_north_index[-1])
+				top_index=game_state.die_top_index,
+				north_index=game_state.die_north_index)
 			die_top_value = game_state.die_face_values.get(die_top_index, 0)
 			if board_value is 0 or die_top_value == board_value:
-				modifiers.append(GameStateModifier(die_north_index=die_north_index,
-				                                   die_top_index=die_top_index,
-				                                   die_location=(new_row, new_col),
-				                                   visited=((new_row, new_col), die_top_index)))
+				modifiers.append((die_north_index,
+								  die_top_index,
+								  (new_row, new_col),
+								  None))
 			elif die_top_value is 0:
-				modifiers.append(GameStateModifier(die_north_index=die_north_index,
-				                                   die_top_index=die_top_index,
-				                                   die_location=(new_row, new_col),
-				                                   visited=((new_row, new_col), die_top_index),
-				                                   die_face_values=(die_top_index, board_value)))
+				modifiers.append((die_north_index,
+								  die_top_index,
+								  (new_row, new_col),
+								  board_value))
 		return modifiers
 
 
@@ -115,7 +94,7 @@ def main():
 		[2, 4, 3, 6, 2],
 		[9, 5, 7, 2, 3],
 		[5, 8, 3, 4, 1],
-	]) # 276480
+	])  # 276480
 	big_static = GameStatic([
 		[1, 5, 5, 5, 6, 1, 1, 4, 1, 3, 7, 5],
 		[3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -129,7 +108,7 @@ def main():
 		[3, 0, 6, 3, 6, 5, 4, 3, 4, 5, 0, 1],
 		[6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
 		[2, 1, 6, 6, 4, 5, 2, 1, 1, 1, 7, 1]
-	]) # 3354674163673461017691780032809373762464467910656
+	])  # 3354674163673461017691780032809373762464467910656
 	medium_static = GameStatic([
 		[4, 0, 6, 4, 1, 8, 1, 4, 2, 1, 0, 3],
 		[7, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 2],
@@ -140,7 +119,7 @@ def main():
 		[6, 1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 6],
 		[3, 0, 6, 3, 6, 5, 4, 3, 4, 5, 0, 1],
 
-	])#10223359979859933659136  / 113592888665110373990400000000?
+	])  # 10223359979859933659136  / 113592888665110373990400000000?
 	practise_static = GameStatic([
 		[3, 4, 1, 7, 5],
 		[1, 2, 0, 3, 5],
@@ -149,42 +128,46 @@ def main():
 		[0, 4, 3, 0, 2],
 		[0, 5, 0, 2, 3],
 		[5, 0, 0, 4, 1],
-	]) # 612220032000
+	])  # 816293376000
 	static_one_zero = GameStatic([
 		[3, 4, 1, 7, 5],
 		[1, 2, 0, 3, 5],
 		[2, 4, 3, 6, 2],
 		[9, 5, 7, 2, 3],
 		[5, 8, 3, 4, 1],
-	]) # 622080
-	static = medium_static
-	#static = GameStatic([[0, 0, 0]])
+	])  # 622080
+	static = big_static
+	# static = GameStatic([[0, 0, 0]])
 
-	initial_state = GameState(die_north_index=[2],
-	                          die_top_index=[1],
-	                          die_location=[(0, 0)],
-	                          visited=OrderedDict([((0, 0), 1)]),
-	                          die_face_values=OrderedDict([(1, static.board[0][0])]))
+	initial_state = GameState(die_north_index=2,
+							  die_top_index=1,
+							  die_location=(0, 0),
+							  visited=OrderedDict([((0, 0), 1)]),
+							  die_face_values=OrderedDict([(1, static.board[0][0])]))
 
 	best_solution = [None]
+
 	def solve(game_static, game_state):
-		if game_state.die_location[-1] == game_static.end_location:
-			# if SOLUTION:
-			# 	logger.warning('Comparing new solution:\n%s\nto existing:\n%s', game_state, SOLUTION)
+		if game_state.die_location == game_static.end_location:
 			if best_solution[0] is None or game_state.score() > best_solution[0].score():
 				best_solution[0] = deepcopy(game_state)
 		next_state_modifiers = game_static.next_state_modifiers(game_state)
 		if not next_state_modifiers:
 			return None
-		solution = None
-		for modifier in next_state_modifiers:
-			game_state.modify(modifier)
+		for die_north_index, die_top_index, die_location, new_die_face_value in next_state_modifiers:
+			game_state.die_north_index = die_north_index
+			game_state.die_top_index = die_top_index
+			game_state.die_location = die_location
+			game_state.visited[die_location] = die_top_index
+			if new_die_face_value:
+				game_state.die_face_values[die_top_index] = new_die_face_value
 			solve(game_static, game_state)
-			game_state.unmod(modifier)
 
-		return solution
+			game_state.visited.popitem()
+			if new_die_face_value:
+				game_state.die_face_values.popitem()
+
 	solve(static, initial_state)
-
 
 	logger.warning('Solved: %s', best_solution[0])
 
